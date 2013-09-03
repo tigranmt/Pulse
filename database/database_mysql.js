0ba19348a61fa71c795a62be4ea1869a025df848
Tigran Martirosyan <tigranmt@gmail.com>
2013-09-03 12:40:13 +0200

    test commit

--- database/database_mysql.js
+++ database/database_mysql.js
@@ -11,73 +11,21 @@
  var errorsTableName = "Errors";
  var connection = undefined;
 
+var remote = true,
 
 
- function parse_url (str, component) {
-  // http://kevin.vanzonneveld.net
-  // +      original by: Steven Levithan (http://blog.stevenlevithan.com)
-  // + reimplemented by: Brett Zamir (http://brett-zamir.me)
-  // + input by: Lorenzo Pisani
-  // + input by: Tony
-  // + improved by: Brett Zamir (http://brett-zamir.me)
-  // %          note: Based on http://stevenlevithan.com/demo/parseuri/js/assets/parseuri.js
-  // %          note: blog post at http://blog.stevenlevithan.com/archives/parseuri
-  // %          note: demo at http://stevenlevithan.com/demo/parseuri/js/assets/parseuri.js
-  // %          note: Does not replace invalid characters with '_' as in PHP, nor does it return false with
-  // %          note: a seriously malformed URL.
-  // %          note: Besides function name, is essentially the same as parseUri as well as our allowing
-  // %          note: an extra slash after the scheme/protocol (to allow file:/// as in PHP)
-  // *     example 1: parse_url('http://username:password@hostname/path?arg=value#anchor');
-  // *     returns 1: {scheme: 'http', host: 'hostname', user: 'username', pass: 'password', path: '/path', query: 'arg=value', fragment: 'anchor'}
-  var query, key = ['source', 'scheme', 'authority', 'userInfo', 'user', 'pass', 'host', 'port',
-            'relative', 'path', 'directory', 'file', 'query', 'fragment'],
-    ini = (this.php_js && this.php_js.ini) || {},
-    mode = (ini['phpjs.parse_url.mode'] &&
-      ini['phpjs.parse_url.mode'].local_value) || 'php',
-    parser = {
-      php: /^(?:([^:\/?#]+):)?(?:\/\/()(?:(?:()(?:([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?))?()(?:(()(?:(?:[^?#\/]*\/)*)()(?:[^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-      strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-      loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/\/?)?((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/ // Added one optional slash to post-scheme to catch file:/// (should restrict this)
-    };
 
-  var m = parser[mode].exec(str),
-    uri = {},
-    i = 14;
-  while (i--) {
-    if (m[i]) {
-      uri[key[i]] = m[i];
-    }
-  }
+return; 
 
-  if (component) {
-    return uri[component.replace('PHP_URL_', '').toLowerCase()];
-  }
-  if (mode !== 'php') {
-    var name = (ini['phpjs.parse_url.queryKey'] &&
-        ini['phpjs.parse_url.queryKey'].local_value) || 'queryKey';
-    parser = /(?:^|&)([^&=]*)=?([^&]*)/g;
-    uri[name] = {};
-    query = uri[key[12]] || '';
-    query.replace(parser, function ($0, $1, $2) {
-      if ($1) {uri[name][$1] = $2;}
-    });
-  }
-  delete uri.source;
-  return uri;
-}
+if(remote) {
 
 
- var serverServices = process.env.VCAP_SERVICES;
- if(serverServices) {
- 	
- 	 var env = JSON.parse(process.env.VCAP_SERVICES);
-     var credentials = env['mysql 5.1'][0]['credentials'];
- 	//remote connection
- 	connection = mysql.createConnection({
- 		host     :  credentials.hostname,
-  		user     :  credentials.user,
-  		password :  credentials.password,
-  		port     :  credentials.port 
+	//remote connection
+	connection = mysql.createConnection({
+		host     :  'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/',
+		user     :  'adminSJAsrB5',
+		password :  '8_kdJ-tNyGe-',		
+		database :  'pulse'
 	});
 
  }
@@ -98,7 +46,7 @@
 
 connection.connect(function(err) {
      if(err != null) {
-     	throw err;
+     	console.log(err);
      }
     
 });
@@ -144,6 +92,7 @@ connection.query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHE
 													"ID MEDIUMINT NOT NULL AUTO_INCREMENT," + 
 												    "PRIMARY KEY (ID));";
 
+ 					console.log("Create " + clientsTableName + " table");
 
 					connection.query(createClientTable, function(err){
 								if (err) { throw err; }
